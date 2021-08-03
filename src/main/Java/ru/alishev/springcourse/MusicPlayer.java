2471e34d7011cbc4ffc3f3121125710e08750525
Voynov <thevoynov@yandex.ru>
2021-08-03 15:31:23 +0300

    Annotation

--- src/main/Java/ru/alishev/springcourse/MusicPlayer.java
+++ src/main/Java/ru/alishev/springcourse/MusicPlayer.java
@@ -23,10 +23,13 @@ public class MusicPlayer {
 //        this.music = music;
 //    }
 
-    public void playMusic() {
-        System.out.println("Playing: " +
-                classicalMusic.getSong());
-        System.out.println("Playing: " +
-                rockMusic.getSong());
+    public String playMusic() {
+        return "Playing: " +
+                classicalMusic.getSong();
+
+//        System.out.println("Playing: " +
+//                classicalMusic.getSong());
+//        System.out.println("Playing: " +
+//                rockMusic.getSong());
     }
 }
